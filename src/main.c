59623800c5a0cb9f7e156e3d95e02889b15b5be6
EliotFerragni <eliot.ferragni@epfl.ch>
2020-06-16 15:23:26 +0200

    -Added LED thread to know it's running

--- src/main.c
+++ src/main.c
@@ -12,7 +12,81 @@
 #include "ch.h"
 #include "hal.h"
 
+static THD_WORKING_AREA(waBlinker,128);
+static THD_FUNCTION(Blinker,arg) {
+	(void)arg;
+	chRegSetThreadName("blinker");
+	while(true){
+		// palClearLine(LINE_STATUS_LED1_R);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED2_R);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED3_R);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED1_G);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED2_G);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED3_G);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED1_B);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED2_B);
+		// chThdSleepMilliseconds(50);
+		// palClearLine(LINE_STATUS_LED3_B);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED1_R);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED2_R);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED3_R);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED1_G);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED2_G);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED3_G);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED1_B);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED2_B);
+		// chThdSleepMilliseconds(50);
+		// palSetLine(LINE_STATUS_LED3_B);
+		// chThdSleepMilliseconds(50);
 
+		palClearLine(LINE_FRONT_LED_R);
+		palClearLine(LINE_RIGHT_LED_R);
+		palClearLine(LINE_REAR_LED_R);
+		palClearLine(LINE_LEFT_LED_R);
+		chThdSleepMilliseconds(500);
+		palSetLine(LINE_FRONT_LED_R);
+		palSetLine(LINE_RIGHT_LED_R);
+		palSetLine(LINE_REAR_LED_R);
+		palSetLine(LINE_LEFT_LED_R);
+		chThdSleepMilliseconds(500);
+		palClearLine(LINE_FRONT_LED_G);
+		palClearLine(LINE_RIGHT_LED_G);
+		palClearLine(LINE_REAR_LED_G);
+		palClearLine(LINE_LEFT_LED_G);
+		chThdSleepMilliseconds(500);
+		palSetLine(LINE_FRONT_LED_G);
+		palSetLine(LINE_RIGHT_LED_G);
+		palSetLine(LINE_REAR_LED_G);
+		palSetLine(LINE_LEFT_LED_G);
+		chThdSleepMilliseconds(500);
+		palClearLine(LINE_FRONT_LED_B);
+		palClearLine(LINE_RIGHT_LED_B);
+		palClearLine(LINE_REAR_LED_B);
+		palClearLine(LINE_LEFT_LED_B);
+		chThdSleepMilliseconds(500);
+		palSetLine(LINE_FRONT_LED_B);
+		palSetLine(LINE_RIGHT_LED_B);
+		palSetLine(LINE_REAR_LED_B);
+		palSetLine(LINE_LEFT_LED_B);
+		chThdSleepMilliseconds(500);
+		
+	}
+}
 
 int main(void) {
 	
@@ -26,6 +100,7 @@ int main(void) {
 	halInit();
 	chSysInit();
 
+	chThdCreateStatic(waBlinker, sizeof(waBlinker), NORMALPRIO, Blinker, NULL);
   
 	while (true){
 		chThdSleepMilliseconds(500);
