ab90cb4ea7f53f98f322152dc40e4f6a980668cb
EliotFerragni <eliot.ferragni@epfl.ch>
2020-06-17 09:13:36 +0200

    -Minimal config of IMU to read its outputs

--- src/main.c
+++ src/main.c
@@ -117,23 +117,45 @@ int main(void) {
 
 	chThdCreateStatic(waBlinker, sizeof(waBlinker), NORMALPRIO, Blinker, NULL);
 
-	uint8_t txbuf[2] = {0x80 | 0x0F, 0}; // read register 0x0F
-	uint8_t rxbuf[2] = {0};
+	uint8_t txbuf[30];
+	uint8_t rxbuf[30];
+
+	/* IMU CONFIG */
+
+	txbuf[0] = 0x10; // write register 0x10 (CTRL1_XL)
+	txbuf[1] = 0xA0; // write high performance mode accelerometer 2g
+	spiAcquireBus(&SPID2);
+	spiStart(&SPID2, &spicfg_imu);
+	spiSelect(&SPID2);
+	spiExchange(&SPID2, 2, txbuf, rxbuf);
+	spiUnselect(&SPID2);
+	spiReleaseBus(&SPID2);
+
+	txbuf[0] = 0x11; // write register 0x11 (CTRL2_G)
+	txbuf[1] = 0xA0 | 0x0C; // write high performance mode gyroscope 2000dps
+	spiAcquireBus(&SPID2);
+	spiStart(&SPID2, &spicfg_imu);
+	spiSelect(&SPID2);
+	spiExchange(&SPID2, 2, txbuf, rxbuf);
+	spiUnselect(&SPID2);
+	spiReleaseBus(&SPID2);
   
 	while (true){
-		chThdSleepMilliseconds(500);
+		chThdSleepMilliseconds(100);
 
-		/* IMU */
-		txbuf[0] = 0x80 | 0x0F;
+		/* IMU READING */
+		txbuf[0] = 0x80 | 0x22; // read register 0x22
 		txbuf[1] = 0;
 		spiAcquireBus(&SPID2);
 		spiStart(&SPID2, &spicfg_imu);
 		spiSelect(&SPID2);
-		spiExchange(&SPID2, 2, txbuf, rxbuf);
+		spiExchange(&SPID2, 1+12, txbuf, rxbuf);
 		spiUnselect(&SPID2);
 		spiReleaseBus(&SPID2);
-
-		chprintf((BaseSequentialStream *)&SD5, "Register : %d %d \r\n", rxbuf[0], rxbuf[1]);
+		chprintf((BaseSequentialStream *)&SD5, "gyroscope:         X       Y       Z\r\n");
+		chprintf((BaseSequentialStream *)&SD5, "                 %5d     %5d     %5d\r\n", (int16_t)(rxbuf[1] | rxbuf[2]<<8), (int16_t)(rxbuf[3] | rxbuf[4]<<8), (int16_t)(rxbuf[5] | rxbuf[6]<<8));
+		chprintf((BaseSequentialStream *)&SD5, "Accelerometer:     X       Y       Z\r\n");
+		chprintf((BaseSequentialStream *)&SD5, "                 %5d     %5d     %5d\r\n\r\n", (int16_t)(rxbuf[7] | rxbuf[8]<<8), (int16_t)(rxbuf[9] | rxbuf[10]<<8), (int16_t)(rxbuf[11] | rxbuf[12]<<8));
 
 		/* IMU */
 		// txbuf[0] = 0x80 | 0x0F;
